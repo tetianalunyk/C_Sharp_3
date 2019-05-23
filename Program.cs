bdb169a9b1ff2f43d45f09d0130474e56fbb6562
tetianalunyk <44263569+tetianalunyk@users.noreply.github.com>
2019-05-23 17:33:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StudentCollection studentCollection = new StudentCollection();
+            studentCollection.AddDefaults();
+            Console.WriteLine("First task: Collection of students - " + studentCollection);
+            /*studentCollection.AddStudents(
+                    TestCollections.GetStudent(3),
+                    TestCollections.GetStudent(2),
+                    TestCollections.GetStudent(5),
+                    TestCollections.GetStudent(1),
+                    TestCollections.GetStudent(4)
+                    
+                );
+
+            Console.WriteLine("First task: Collection of students - " + studentCollection);
+            studentCollection.SortBySurname();
+            Console.WriteLine("\n\nSecond task:\nSorted By Surname:\n" + studentCollection);
+            studentCollection.SortByDate();
+            Console.WriteLine("\nSorted By Date:\n" + studentCollection);
+            studentCollection.SortByEverage();
+            Console.WriteLine("\nSorted By Everage:\n" + studentCollection);
+
+            Console.WriteLine("\n\nThird task:\nGet Max Everage: " + studentCollection.GetMaxEvarage());
+            int value = 3;
+            Console.WriteLine("\nEverage mark in group:\n" + string.Join(" ; ", studentCollection.EvarageMarkGroup(value).Select(x => x.GetEverage).ToArray()));
+            Console.WriteLine("\nPeople with Master degree:\n" + string.Join(" ; ", studentCollection.GetListOfMasters().Select(x => x._surname).ToArray()));
+
+            Console.WriteLine("\n\nFourth task:\n");
+            TestCollections test = new TestCollections(10);
+            Console.WriteLine("Searching time:");
+            test.MeasureTime();
+
+
+
+
+            /*Console.WriteLine("\nFirst task - two objects: ");
+            DateTime date = new DateTime(1999, 09, 24);
+            Person Tania = new Person("Tetiana", "Lunyk", date);
+            Person Tetiana = new Person("Tetiana", "Lunyk", date);
+            Console.WriteLine("== " + (Tania == Tetiana));
+            Console.WriteLine("Equals: " + Tetiana.Equals(Tania));
+            Console.WriteLine("Hash : " + Tania.GetHashCode() + " " + Tetiana.GetHashCode());
+
+            Console.WriteLine("\nSecond task - object Student: ");
+            Student Lunyk = new Student("Tetiana", "Lunyk", date, Education.Bachelor, 311);
+            Exam[] first = new Exam[2];
+            first[0] = new Exam("Programming", 5, new DateTime(1999, 09, 24));
+            first[1] = new Exam("Math", 2, new DateTime(1999, 09, 24));
+            Lunyk.AddExams(first);
+            Test[] first_t = new Test[1];
+            first_t[0] = new Test("Java", true);
+            Lunyk.AddCredits(first_t);
+            Console.WriteLine(Lunyk.ToString());
+            Console.WriteLine("\nThird task show Person from Student : ");
+            Console.WriteLine(Lunyk.PersonBase);
+
+            Console.WriteLine("\nFifth task exception : ");
+            try
+            {
+                Lunyk.group = -1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+            Console.WriteLine("\nDeep copy: ");
+            Student Me = (Student)Lunyk.DeepCopy();
+            Console.WriteLine(Me.Equals(Lunyk));
+            Lunyk._Exam[0].subject = "kjhgf";
+            Exam[] second = new Exam[1];
+             second[0] = new Exam("C_sharp", 3, new DateTime(1999, 09, 24));
+             Lunyk.AddExams(second);
+            Console.WriteLine(Me + "" + Lunyk);
+
+            Console.WriteLine("\nSixth task foreach Papers :");
+            foreach (Exam ex in Lunyk.GetExamsMoreThen(3))
+            {
+                Console.WriteLine(ex);
+            }
+            string str;
+            str=Console.ReadLine();*/
+        }
+    }
+}
---
+++ StudentCollection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class StudentCollection
+    {
+        private List<Student> students;
+        public void AddDefaults()
+        {
+            Student[] defaul = new Student[2];
+            DateTime date = new DateTime(1999, 09, 24);
+            defaul[0] = new Student(new Person("Tetiana", "Lunyk", date), Education.Bachelor, 311, new List<Test> { new Test("Java", true)}, new List<Exam> { new Exam("Programming", 5, new DateTime(1999, 09, 24))});
+            defaul[1] = new Student(new Person("Tetiana", "Kukhta", date), Education.Bachelor, 311, new List<Test> { new Test("Java", false) }, new List<Exam> { new Exam("Programming", 3, new DateTime(1999, 09, 24)) });
+            students.AddRange(defaul);
+        }
+
+        public void AddStudents(params Student[] range)
+        {
+            students = new List<Student>();
+            foreach(var stud in range)
+            {
+               students.Add(stud);
+            }
+        }
+
+        public override string ToString()
+        {
+            string temp = "";
+            for (int i = 0; i < students.Count; i++)
+                temp += "\nName " + students[i]._name + "\nSurname " + students[i]._surname + "\nDate of birth: " + students[i]._dateOfBirth + students[i].ToString() + "\r\n";
+            return "Students: " + temp;
+        }
+
+        public virtual string ToShortString()
+        {
+            string temp = "";
+            for (int i = 0; i < students.Count; i++)
+                temp += "\nName: " + students[i]._name + "\nSurname: " + students[i]._surname + "\nDate of Birth: " + students[i]._dateOfBirth +"\nEducation: "+ students[i]._educ +"\nGroup: "+ students[i].group + "\nCount exams: "+ students[i]._Exam.Count + "\nEveradge: " + students[i].GetEverage + "\nCount tests: " + students[i]._Test.Count + "\r\n";
+            return "Students: " + temp;
+        }
+
+        public void SortBySurname() => students.Sort();// (x,y) => x.CompareTo(y));
+        
+
+        public void SortByDate()
+        {
+            students.Sort((x, y) => new Student().Compare(x,y));
+        }
+
+        public void SortByEverage()
+        {
+            students.Sort((x, y) => new AverageMarkComparer().Compare(x, y));
+        }
+
+        public double GetMaxEvarage()
+        {
+            return students.Count != 0 ? students.Select(x => x.GetEverage).Max() : 0;
+        }
+
+        public List<Student> EvarageMarkGroup(double value)
+        {
+            return students.Where(x => Math.Abs(x.GetEverage - value)<0.001).ToList();
+        }
+
+        public IEnumerable<Student> GetListOfMasters()
+        {
+            return students.Where(x => x._educ == Education.Master);
+        }
+
+
+    }
+}
